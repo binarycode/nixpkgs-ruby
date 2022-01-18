95084804ace4e3977330ab9d850072cb2ab01a7f
Igor Sidorov <igor.sidorov@binarycode.ru>
2022-01-18 16:09:04 +0300

    add 3.0.3 with invalid sha256

---
+++ versions/3/0/3/meta.nix
@@ -0,0 +1,11 @@
+{
+  versionName = "3.0.3";
+  version = [
+    "3"
+    "0"
+    "3"
+    ""
+  ];
+  url = https://cache.ruby-lang.org/pub/ruby/ruby-3.0.3.tar.gz;
+  sha256 = "0000000000000000000000000000000000000000000000000000000000000000";
+}
--- versions/3/0/default.nix
+++ versions/3/0/default.nix
@@ -1,5 +1,6 @@
 rec {
   "0" = import ./0;
   "1" = import ./1;
-  "*" = import ./1;
+  "3" = import ./3;
+  "*" = import ./3;
 }
